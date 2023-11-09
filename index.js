fc1f012121d1adab148e7e5698f75dab3b5e9b42
TMethot01 <tylermethot@hotmail.com>
2023-11-09 14:38:20 -0500

    new app

--- index.js
+++
@@ -1,6 +0,0 @@
-import {createRoot} from "react-dom/client";
-import ReactDOM from "react-dom";
-import App from './App.js'
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />)
