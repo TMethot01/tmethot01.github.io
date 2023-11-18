def58db8a4457a17ff5687199d7ccd8a1a2dc9c7
TMethot01 <tylermethot@hotmail.com>
2023-11-17 19:06:28 -0500

    Changed contact info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,8 +3,16 @@ import React from 'react';
  
 const Contact = () => {
     return (
-        <div>
-            <h1>Send me an email!</h1>
+        <div id="about-wrapper">
+            <div id="about">
+                <h1>
+                    Contact Me
+                </h1>
+                <h3 style={{textAlign: "center"}}>
+                    Feel free to contact me about any business inquiries through
+                     whatever channel brought you to the website.
+                </h3>
+            </div>
         </div>
     );
 };
